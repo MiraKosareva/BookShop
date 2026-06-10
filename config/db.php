2b39e5d134cfc5a05719125cff8bd8a9dbbb6771
MiraKosareva <kosarevamiro@gmail.com>
2026-06-10 13:32:51 +0300

    Небольшие правки

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;port=3308;dbname=kosarevam2',
-    'username' => 'kosarevam2',
-    'password' => 'W@5Gm_CnnuDyEUy',
+    'dsn' => 'mysql:host=localhost;dbname=BookShop',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
